1ca9bdf23193b7ae5ceb5c7feee6c32162609bca
syal khan <sayalafridai@gmail.com>
2023-03-14 14:06:41 +0500

    template parts

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Main template file.
  * 
@@ -9,66 +10,63 @@
 get_header();
 ?>
 <div id="primary">
-  <main id="main" class="site-main mt-5" role= "main">
+  <main id="main" class="site-main mt-5" role="main">
     <?php
-    if ( have_posts() ){
-      ?>
+    if (have_posts()) {
+    ?>
       <div class="container">
-        <?php 
+        <?php
 
         // Title of the page.
-        if( is_home() && ! is_front_page() ) {
-          ?>
+        if (is_home() && !is_front_page()) {
+        ?>
           <header class="mb-5">
             <h1 class="page-title screen-reader-text">
               <?php single_post_title(); ?>
             </h1>
 
           </header>
-          <?php
+        <?php
         }
         ?>
         <div class="row">
-        <?php
-        $index = 0;
-        $no_of_columns = 3;
-
-
-        while ( have_posts() ): the_post();
-        if ( 0 === $index % $no_of_columns){
-          ?>
-          <div class="col-lg-4 col-md-6 col-sm-12">
           <?php
-        }
-        ?>
-        <h3><?php the_title(); ?></h3>
-        <div><?php the_excerpt(); ?></div>
-        <?php
-        $index ++;
+          $index = 0;
+          $no_of_columns = 3;
 
-        if (0 !== $index && 0 === $index % $no_of_columns ){
+
+          while (have_posts()) : the_post();
+            if (0 === $index % $no_of_columns) {
           ?>
-          </div>
-          <?php
-        }
-        
+              <div class="col-lg-4 col-md-6 col-sm-12">
+              <?php
+            }
+              get_template_part('template-parts/content');
+              $index++;
 
-    endwhile;
-    ?>
+              if (0 !== $index && 0 === $index % $no_of_columns) {
+              ?>
+              </div>
+          <?php
+              }
+
+
+            endwhile;
+          ?>
         </div>
-         
-       
 
-        
+
+
+
       </div>
 
-      <?php
+    <?php
     }
     ?>
-    
-    
+
+
 
   </main>
 </div>
-  <?php 
-  get_footer(); ?>
+<?php
+get_footer(); ?>
---
+++ template-parts/content.php
@@ -0,0 +1,9 @@
+<?php 
+/**
+ * Content template
+ * 
+ * @package mytheme2
+ */
+?>
+<h3><?php the_title(); ?></h3>
+<div><?php the_excerpt(); ?></div>
